845c1ffcbbaa1bd5b9597a49dc0258c8a744988a
Nathan L Smith <nlloyds@gmail.com>
2011-04-01 15:07:13 -0500

    pare down tests

--- test/test-client.js
+++ test/test-client.js
@@ -1,10 +1,7 @@
 var gearman = require("gearman"),
     Client = gearman.Client;
 
-exports["Constructor"] = function (test) {
-    test.equal(typeof Client, "function", "Client is a constructor");
-    test.equal(typeof new Client(), "object", "new Client() creates an object");
-    test.equal(typeof Client(), "object", "Constructor constructs without 'new'");
-    test.equal(typeof gearman.createClient(), "object", "gearman.createClient() constructs");
+exports["createClient"] = function (test) {
+    test.ok(gearman.createClient() instanceof Client, "gearman.createClient() creates a client");
     test.done();
 };
