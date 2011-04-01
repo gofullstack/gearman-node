dfe15ad17f61763cae13da8a465135a32a69e3de
Nathan L Smith <nlloyds@gmail.com>
2011-04-01 15:25:10 -0500

    submitjob method

--- test/test-client.js
+++ test/test-client.js
@@ -5,3 +5,9 @@ exports["createClient"] = function (test) {
     test.ok(gearman.createClient() instanceof Client, "gearman.createClient() creates a client");
     test.done();
 };
+
+exports["submitJob"] = function (test) {
+    var client = gearman.createClient();
+    test.equal("object", typeof client.submitJob(), "client.submitJob() creates an object");
+    test.done();
+};
