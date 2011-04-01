3b9e0b07e1cb240e86f99a89b0049a6e82651b55
Nathan L Smith <nlloyds@gmail.com>
2011-04-01 15:05:07 -0500

    remove connection stuff for now

--- test/test-client.js
+++ test/test-client.js
@@ -6,6 +6,5 @@ exports["Constructor"] = function (test) {
     test.equal(typeof new Client(), "object", "new Client() creates an object");
     test.equal(typeof Client(), "object", "Constructor constructs without 'new'");
     test.equal(typeof gearman.createClient(), "object", "gearman.createClient() constructs");
-    test.equal("test", gearman.createClient(["test"]).servers[0].host, "servers property created");
     test.done();
 };
