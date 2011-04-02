04c41236288baddc9b1b57c58224ad68b8a9ae51
Nathan L Smith <nlloyds@gmail.com>
2011-04-02 00:10:54 -0500

    client args and connection setup

--- test/test-client.js
+++ test/test-client.js
@@ -3,7 +3,20 @@ var gearman = require("gearman"),
     Job = gearman.Job;
 
 exports["createClient"] = function (test) {
-    test.ok(gearman.createClient() instanceof Client, "gearman.createClient() creates a client");
+    var client = gearman.createClient();
+    test.ok(client instanceof Client, "gearman.createClient() creates a client");
+
+    test.equal(typeof client.connections, "object", "client has a connections object");
+    test.equal(Object.keys(client.connections)[0], "localhost:4730", "first default connection key is localhost:4730");
+    test.equal(typeof client.connections["localhost:4730"], "object", "first default connection value is an object");
+
+    client = gearman.createClient("example.com:9999");
+    test.equal(Object.keys(client.connections)[0], "example.com:9999", "connection key from string");
+
+    client = gearman.createClient(["example.com:9999", "example2.com:8888"]);
+    test.equal(Object.keys(client.connections)[0], "example.com:9999", "connection key from array");
+    test.equal(Object.keys(client.connections)[1], "example2.com:8888", "2nd connection key from array");
+
     test.done();
 };
 
