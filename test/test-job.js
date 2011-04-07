ba30de91b539d6463c8aed8ec8edc4ba0b58455d
Nathan L Smith <nlloyds@gmail.com>
2011-04-07 14:18:40 -0500

    test tweak

--- test/test-job.js
+++ test/test-job.js
@@ -7,20 +7,20 @@ exports["Job"] = function (test) {
     test.done();
 };
 
-// XXX: These need a real gearman server running on localhost:4730. Need to make
-//      a mock server
+// XXX: These need a real gearman server running on localhost:4730 and
+// fixtures/worker.rb running. Need to make a mock server or something.
 
 exports["submit"] = function (test) {
     var job = new Job({ name: "test" });
     job.submit();
     job.on("create", function (handle) {
-        test.ok(typeof job.handle === "string", "handle returned on create event");
+        test.ok(typeof handle === "string", "handle returned on create event");
+        test.equal(job.handle, handle, "job handle assigned on create event");
         test.done();
     });
 };
 
 exports["event: complete"] = function (test) {
-    // reverse_string is from https://github.com/gearman-ruby/gearman-ruby/blob/master/examples/worker_reverse_string.rb
     var job = new Job({ name: "test", data: "test", encoding: "utf8" });
     job.submit();
     job.on("complete", function (result) {
