58b9fd8ad7e62fb7305205400cc1ed57ed8f1c02
Nathan L Smith <nlloyds@gmail.com>
2011-04-06 23:10:02 -0500

    JOB_CREATED and WORK_COMPLETE working

--- test/test-job.js
+++ test/test-job.js
@@ -7,3 +7,24 @@ exports["Job"] = function (test) {
     test.done();
 };
 
+// XXX: These need a real gearman server running on localhost:4730. Need to make
+//      a mock server
+
+exports["submit"] = function (test) {
+    var job = new Job({ name: "test" });
+    job.submit();
+    job.on("create", function (handle) {
+        test.ok(typeof job.handle === "string", "handle returned on create event");
+        test.done();
+    });
+};
+
+exports["event: complete"] = function (test) {
+    // reverse_string is from https://github.com/gearman-ruby/gearman-ruby/blob/master/examples/worker_reverse_string.rb
+    var job = new Job({ name: "reverse_string", data: "test", encoding: "utf8" });
+    job.submit();
+    job.on("complete", function (result) {
+        test.equal("tset", result, "work completes");
+        test.done();
+    });
+};
