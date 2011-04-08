09dc735484e4b004045cad7f5ad136236ad6bad3
Nathan L Smith <nlloyds@gmail.com>
2011-04-07 23:23:40 -0500

    dry tests

--- test/test-client.js
+++ test/test-client.js
@@ -1,21 +1,17 @@
 var gearman = require("gearman"),
     Client = gearman.Client,
     Job = gearman.Job,
-    testCase = require("nodeunit").testCase;
+    testCase = require("nodeunit").testCase,
+    client = gearman.createClient();
 
 module.exports = testCase({
-    setUp: function (callback) {
-        this.client = gearman.createClient();
-        callback();
-    },
-
     "createClient": function (test) {
-        test.ok(this.client instanceof Client,
+        test.ok(client instanceof Client,
                 "gearman.createClient() creates a client");
-        test.equal(this.client.port, 4730, "default port 4730");
-        test.equal(this.client.host, "localhost", "default host 'localhost'");
+        test.equal(client.port, 4730, "default port 4730");
+        test.equal(client.host, "localhost", "default host 'localhost'");
 
-        var client = gearman.createClient(1234, "test");
+        client = gearman.createClient(1234, "test");
 
         test.equal(client.port, 1234, "port argument 1234");
         test.equal(client.host, "test", "host argument 'test'");
@@ -24,13 +20,13 @@ module.exports = testCase({
     },
 
     "submitJob": function (test) {
-        test.ok(this.client.submitJob("test") instanceof Job,
+        test.ok(client.submitJob("test") instanceof Job,
                 "client.submitJob() creates a job");
         test.done();
     },
 
     "end": function (test) {
-        test.ok(typeof this.client.end === "function",
+        test.ok(typeof client.end === "function",
                 "client.end() method exists");
         test.done();
     }
--- test/test-job.js
+++ test/test-job.js
@@ -1,26 +1,23 @@
 var gearman = require("gearman"),
     Job = gearman.Job,
     EventEmitter = require("events").EventEmitter,
-    testCase = require("nodeunit").testCase;
+    testCase = require("nodeunit").testCase,
+    job = new Job({ name: "test", data: "test", encoding: "utf8" });
+
 
 // XXX: These need a real gearman server running on localhost:4730 and
 // fixtures/worker.rb running. Need to make a mock server or something.
 
-module.exports = testCase({
-    setUp: function (callback) {
-        this.job = new Job({ name: "test", data: "test", encoding: "utf8" });
-        this.job.submit();
-        callback();
-    },
+job.submit();
 
+module.exports = testCase({
     "Job": function (test) {
-        test.ok(new Job() instanceof EventEmitter,
+        test.ok(job instanceof EventEmitter,
                 "Job instances are EventEmitters");
         test.done();
     },
 
     "submit": function (test) {
-        var job = this.job;
         job.on("create", function (handle) {
             test.ok(typeof handle === "string",
                     "handle returned on create event");
@@ -31,14 +28,14 @@ module.exports = testCase({
     },
 
     "event: data": function (test) {
-        this.job.on("data", function (result) {
+        job.on("data", function (result) {
             test.equal("test", result, "work data sent");
             test.done();
         });
     },
 
     "event: complete": function (test) {
-        this.job.on("complete", function (result) {
+        job.on("complete", function (result) {
             test.equal("tset", result, "work completes");
             test.done();
         });
